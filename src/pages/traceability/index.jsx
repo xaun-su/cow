f787954477ea7812fac009d5d95452fa8f2bb0d1
JunYuan <15383083+f1927865184@user.noreply.gitee.com>
2025-04-27 15:50:53 +0800

    feat:溯源信息-疫苗信息完成

--- src/pages/traceability/index.jsx
+++ src/pages/traceability/index.jsx
@@ -6,7 +6,8 @@ import imge from '../../static/images/animal.png';
 // 导入需要的 NutUI 组件，包括 ConfigProvider, Cell, CellGroup, 以及图标
 import { Button, Cell, ConfigProvider, CellGroup, Tabs } from '@nutui/nutui-react-taro';
 // 导入 NutUI 图标，例如 Battery
-import { Add } from '@nutui/icons-react-taro'; // 导入电池图标
+import { Add, ArrowRight } from '@nutui/icons-react-taro'; // 导入电池图标
+import { Steps, Step } from '@nutui/nutui-react-taro'
 
 const AnimalDetails = () => {
   const [tab2value, setTab2value] = useState('0');
@@ -112,15 +113,68 @@ const AnimalDetails = () => {
             </Tabs.TabPane>
             <Tabs.TabPane title='健康信息' value={tabValues.healthInfo}>
               {' '}
-              Tab 2{' '} 
+              Tab 2{' '}
             </Tabs.TabPane>
             <Tabs.TabPane title='检疫信息' value={tabValues.quarantineInfo}>
               {' '}
               Tab 3{' '}
             </Tabs.TabPane>
             <Tabs.TabPane title='疫苗信息' value={tabValues.vaccineInfo}>
-              {' '}
-              Tab 2{' '}
+              <Cell title='证明材料' radius={0} style={
+                {
+                  '--nutui-cell-background-color': '#f5f5f5',
+                }
+              } />
+              <Cell title='动物诊疗许可证' extra={<ArrowRight />} radius={0} />
+              <Cell title='动物防疫合格证' extra={<ArrowRight />} radius={0} />
+              <Cell title='疫苗生产批次和有效期证明' extra={<ArrowRight />} radius={0} />
+              <Cell title='疫苗运输和储存温度记录' extra={<ArrowRight />} radius={0} />
+              <Cell title='免疫接种记录' extra={<ArrowRight />} radius={0} />
+              <Cell title='疫苗记录' radius={0} style={
+                {
+                  '--nutui-cell-background-color': '#f5f5f5',
+                }
+              } />
+              <div style={{
+                height: '300px', padding: '15px 30px',
+                '--nutui-steps-process-icon-before-bg-color': '#e2f9f2', // 进行中点状进度条点的外边颜色
+                '--nutui-steps-process-icon-bg-color': '#e2f9f2', // 进行中icon容器背景色
+                '--nutui-steps-process-icon-color': '#70ba77', // 进行中icon容器字体颜色
+                '--nutui-steps-finish-icon-bg-color': '#e2f9f2', // 完成状态icon 容器的背景色
+                '--nutui-steps-finish-icon-color': '#70ba77', // 完成状态icon 容器的字体颜色
+                '--nutui-steps-finish-line-background': '#a5a5a5', // 完成状态分割线的颜色
+                '--nutui-steps-base-description-color': '#666666', // 描述文案的字体颜色
+                '--nutui-steps-dot-icon-width': '8px', // 点状进度条点的宽度
+                '--nutui-steps-dot-icon-height': '8px', // 点状进度条点的高度
+                '--nutui-steps-dot-icon-border': 'none', // 点状进度条点的边框
+                '--nutui-steps-dot-head-margin': '8px 0 0 0', // 点状进度条点的外边距
+                '--nutui-steps-base-title-color': '#a5a5a5',
+              }}>
+                <Steps direction="vertical" dot value={2}>
+                  <Step
+                    value={1}
+                    title="2024-01-15"
+                    description={
+                      <>
+                        <p>疫苗批次号:x1231231231</p>
+                        <p>疫苗名称:请问企鹅</p>
+                        <p>操作人员:xxxx</p>
+                      </>
+                    }
+                  />
+                  <Step
+                    value={2}
+                    title="2024-01-15"
+                    description={
+                      <>
+                        <p>疫苗批次号:x1231231231</p>
+                        <p>疫苗名称:请问企鹅</p>
+                        <p>操作人员:xxxx</p>
+                      </>
+                    }
+                  />
+                </Steps>
+              </div>
             </Tabs.TabPane>
             <Tabs.TabPane title='繁殖信息' value={tabValues.breedingInfo}>
               {' '}

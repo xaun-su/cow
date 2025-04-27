3db57082001cbdd2686f24337b67f8c972fa5035
JunYuan <15383083+f1927865184@user.noreply.gitee.com>
2025-04-27 10:11:12 +0800

    feat:牲畜列表完成

--- src/pages/animal/index.jsx
+++ src/pages/animal/index.jsx
@@ -1,15 +1,49 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.less'
+import React, { useState } from 'react';
+import { SearchBar, Avatar } from '@nutui/nutui-react-taro';
+import { View, Text } from '@tarojs/components'; // 确保导入了 Text
+import { ArrowRight, User,Add } from '@nutui/icons-react-taro'; // 确保导入了 User 和 ArrowRight
+import './index.less';
 
-export default function Category () {
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+const Demo7 = () => {
+  const [value, setValue] = useState('');
 
   return (
-    <View className='category'>
-      <Text>Hello world!</Text>
+    <View className='animal'>
+      {/* SearchBar */}
+      <SearchBar onChange={(val) => setValue(val)} maxLength={10} />
+
+      <View className='category'>
+        {/* 动态生成 category-item */}
+        {Array.from({ length: 5 }, (_, i) => (
+          <View key={i} className='category-item'> {/* 将 className 和 key 放在同一层 View */}
+            {/* 左侧：头像和文本 */}
+            <View className='item-left'> {/* 包裹头像和文本的容器，水平 flex */}
+              {/* 确保导入了 User 图标 */}
+              <Avatar icon={<User/>} size="normal" color="#fff"
+        background="#0bcb77" className='normal'/> {/* 头像 */}
+              <View className='item-text'> {/* 包裹文本的容器，垂直 flex */}
+                <Text className='item-title'>牲畜编号</Text> {/* 标题文本 */}
+                <Text className='item-imei'>IMEI: 1234567890</Text> {/* IMEI 文本 */}
+              </View>
+            </View>
+
+            {/* 右侧：箭头 */}
+            <View className='item-right'> {/* 包裹箭头的容器 */}
+              <ArrowRight /> {/* 箭头图标 */}
+            </View>
+          </View>
+        ))}
+      </View>
+      <Avatar
+        color="#fff"
+        background="#0bcb77"
+        size="large"
+        className='add-btn'
+        icon=<Add />
+      >
+      </Avatar>
     </View>
-  )
-}
+  );
+};
+
+export default Demo7;
